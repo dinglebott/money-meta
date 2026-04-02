a7c0c64cda30eb372438e07b5ff24a48bf189068
yujay <tyujay@gmail.com>
2026-04-02 17:19:33 +0800

    finalise predict.py pipeline

--- README.md
+++ README.md
@@ -12,18 +12,17 @@ This repo was originally intended to be a meta-model that synthesises probabilit
 ## Project structure
 The main code is all in the top-level scripts.\
 The `custom_modules` folder contains helper functions to fetch and manipulate the data.\
-`nnTrainer.py`, `xgbTrainer.py`, and `train_model` are deprecated (see explanatory note above)
-It also contains fully-trained XGBoost and RNN models, and scripts to get leak-free predictions for training the meta-model.
+`nnTrainer.py`, `xgbTrainer.py`, and `train_model` are deprecated (see explanatory note above). They were originally used to get leak-free predictions for training the meta-model.
 
-## How to use
+## Setup
 Inside the `env.json` file, set the current year, the desired instrument, and granularity. I built my model for 2026 EUR/USD at H4 granularity. For other options, set these to the appropriate values.\
 #### IMPORTANT:
 You need an OANDA API key to pull historical data (or you can use the data I pulled already).\
 If you have a key, set it as an environment variable `API_KEY` in a local `.env` file.\
 The code fetches from the api-fxtrade.oanda.com live server, so if your key is from a demo account, change this to api-fxpractice.oanda.com.
 
-## How to use a model
-Run `use_model.py`, the prediction and confidence will be printed to the terminal.\
+## How to use the models
+Run `predict.py`, the predictions and probability breakdowns are printed to the terminal.\
 Remember to set the correct global variables. Obviously make sure you have the correct models trained for your use case first.
 
 ## Why?????
---
+++ predict.py
@@ -0,0 +1,128 @@
+from custom_modules import datafetcher, dataparser
+import xgboost as xgb
+import torch
+import joblib
+import numpy as np
+import os
+import json
+
+# GLOBAL VARIABLES
+with open("env.json", "r") as file:
+    globalVars = json.load(file)
+yearNow, instrument, granularity, xgbVersion, nnVersion = globalVars.values()
+
+# use CUDA if available, otherwise use CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# FETCH AND PARSE CURRENT DATA
+jsonPath = datafetcher.getData(instrument, granularity, 200, "live")
+df = dataparser.parseData(jsonPath)
+
+# XGBOOST SUBMODEL
+xgbModel = xgb.XGBClassifier()
+xgbModel.load_model(os.path.join("submodels", "XGB", f"XGBoost_{instrument}_{granularity}_{yearNow}_v{xgbVersion}.json"))
+# get features
+with open(os.path.join("submodels", "XGB", f"features_v{xgbVersion}.json")) as file:
+    xgbFeatures = json.load(file)["features"]
+
+# GET XGBOOST PREDICTION
+latestCandle = df[xgbFeatures].iloc[[-1]] # slice out last row (last candle)
+xgbPred = xgbModel.predict(latestCandle)[0] # gets the only element of the 1D numpy array [n_samples]
+xgbProbs = xgbModel.predict_proba(latestCandle)[0] # gets the only row of the 2D numpy array [n_samples, n_classes]
+
+# RNN SUBMODEL
+class ForexHybrid(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, dropout, lstm_dropout, output_size,
+                num_filters, kernel_size):
+        super(ForexHybrid, self).__init__()
+        # CNN layers: takes 3D tensor as input (batch_size, channels, length)
+        self.cnn = torch.nn.Sequential(
+            torch.nn.Conv1d(in_channels=input_size, out_channels=num_filters,
+                    kernel_size=kernel_size, padding=kernel_size//2),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(num_filters), # normalise before passing to LSTM
+            torch.nn.Dropout(dropout)
+        )
+        # LSTM layers
+        self.lstm = torch.nn.LSTM(
+            input_size=num_filters, # takes CNN output
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=lstm_dropout if num_layers > 1 else 0
+        )
+        # Output layer (maps final pattern produced by LSTM to actual prediction) (fully connected)
+        self.fc = torch.nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        # x is 3D tensor (batch_size, timesteps, features)
+        # CNN
+        x = x.permute(0, 2, 1) # (batch, features, timesteps)
+        x = self.cnn(x)
+        x = x.permute(0, 2, 1) # (batch, timesteps, num_filters)
+        # LSTM
+        lstmOutput, (hidden, cell) = self.lstm(x)
+        # lstmOutput: hidden state of EVERY timestep for the LAST layer only (batch_size, timesteps, hidden_size)
+        # hidden: final hidden state (LAST timestep) for EVERY layer (layers, batch_size, hidden_size)
+        # cell: similar to hidden but contains cell state instead of hidden state
+        # FC
+        lastTimestep = lstmOutput[:, -1, :] # slice out last timestep across all samples and neurons
+        return self.fc(lastTimestep) # map to prediction (batch_size, output size)
+# get features
+with open(os.path.join("submodels", "NN", f"features_v{nnVersion}.json")) as file:
+    nnFeatures = json.load(file)["features"]
+# get hyperparams
+with open(os.path.join("submodels", "NN", f"hyperparameters_v{nnVersion}.json")) as file:
+    nnHyperparams = json.load(file)
+    nnParams = nnHyperparams["modelParams"]
+    nnLookback = nnHyperparams["lookback"]
+# build model
+nnModel = ForexHybrid(
+    **nnParams,
+    input_size=len(nnFeatures),
+    output_size=3,
+    lstm_dropout=nnParams["dropout"]
+).to(device)
+# load model
+nnFilepath = os.path.join("submodels", "NN", f"NN_{instrument}_{granularity}_{yearNow}_v{nnVersion}.pth")
+nnModel.load_state_dict(torch.load(nnFilepath, map_location=device))
+nnModel.eval()
+scaler = joblib.load(os.path.join("submodels", "NN", f"scaler_v{nnVersion}.pkl"))
+
+# GET RNN PREDICTION
+# construct sequences
+features = scaler.transform(df[nnFeatures])
+X = []
+for i in range(len(features) - nnLookback + 1):
+    X.append(features[i : i + nnLookback])
+X = torch.tensor(np.array(X), dtype=torch.float32, device=device)
+# run inferences
+with torch.no_grad():
+    logits = nnModel(X)
+    allNnProbs = torch.softmax(logits, dim=1).cpu().numpy()
+    allNnPreds = torch.argmax(logits, dim=1).cpu().numpy()
+nnProbs = allNnProbs[-1]
+nnPred = allNnPreds[-1]
+
+# DISPLAY RESULTS
+def getLabel(num):
+    match num:
+        case 0:
+            return "DOWN"
+        case 1:
+            return "FLAT"
+        case 2:
+            return "UP"
+
+print(f"XGB Version: {xgbVersion}")
+print(f"LSTM Version: {nnVersion}")
+print("")
+
+for idx, prob in enumerate(xgbProbs): # round probabilities to 2sf, format with "%"
+    print(f"{getLabel(idx)}: {prob*100:.2f}%") # label each probability and print
+print(f"XGBoost prediction: {getLabel(xgbPred)}")
+print("")
+
+for idx, prob in enumerate(nnProbs):
+    print(f"{getLabel(idx)}: {prob*100:.2f}%")
+print(f"LSTM prediction: {getLabel(nnPred)}")
