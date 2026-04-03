f610baa5cef763de2e59caf6ba20414f77bba69f
yujay <tyujay@gmail.com>
2026-04-03 20:42:43 +0800

    fix data formatting

--- dist/api/models.py
+++ dist/api/models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 class PredictionResponse(BaseModel):
     xgbPred: str # "down" | "flat" | "up"
-    xgbProbs: dict[str, float] # {"down": 0.42, "flat": 0.31, "up": 0.27}
+    xgbProbs: dict[str, float] # {"0": 0.42, "1": 0.31, "2": 0.27}
     nnPred: str
     nnProbs: dict[str, float]
     timestamp: str
